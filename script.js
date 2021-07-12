cc0e5161876d0ebd8d27c30ee5cd15bebedd3663
Codjam <jamaltbutler@gmail.com>
2021-07-12 17:18:21 -0400

    added touch to see if it works on mobile devices

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ for(let i = 0; i < SQUARES; i++) {
 
   square.addEventListener('mouseout', () => removeColor(square))
 
-  square.addEventListener('touchmove', () => setColor(square))
+  square.addEventListener('touch', () => setColor(square))
 
   square.addEventListener('touchcancel', () => removeColor(square))
 
